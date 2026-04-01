188cc4df8271a4a6cbe455ea8a45b764af11c7f0
okaufman <olivia.kaufmann@unibe.ch>
2026-04-01 16:04:28 +0200

    ilias10

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'ucapmo';
 $version = '1.0.0';
 $ilias_min_version = '7.0.0';
-$ilias_max_version = '9.999.999';
+$ilias_max_version = '10.999.999';
 $responsible = 'Timon Amstutz';
 $responsible_mail = 'timon.amstutz@ilub.unibe.ch';
